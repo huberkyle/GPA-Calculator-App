1af85ee11aa464f3e533171badc65848bdc57ad8
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2016-04-06 22:26:21 -0400

    Corrected Files
    Android App Files
      - www Files

---
+++ www/js/calcReplacementCreditsGPA.js
@@ -0,0 +1,27 @@
+/*
+calcReplacementCreditsGPA.js
+By: Kyle, Morgan, Aaron
+This page runs the desired GPA calculations when there are replacement credit hours include.
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+// Calculates desired GPA when there are replacement credits hours included
+function calcReplacementCreditsGPA ()
+{
+}
